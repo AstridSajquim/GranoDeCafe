eb8368d801d07356e045d9c5278f27cb370cfefb
Vetto180 <all180.lg@hotmail.com>
2016-06-15 11:43:41 -0500

    Mi primer commit

--- Interfaces/Interfaces/src/Ventanas/Sales.java
+++ Interfaces/Interfaces/src/Ventanas/Sales.java
@@ -14,6 +14,10 @@ import javax.swing.table.DefaultTableModel;
 /**
  *
  * @author Vetto
+ * 
+ * Si lees esto es porque ya hice mi primer commit en el proyecto, 
+ * Ah! y estoy muerto..... 
+ * 
  */
 public class Sales extends javax.swing.JFrame {
 ConexionSQLbase c=new ConexionSQLbase();
